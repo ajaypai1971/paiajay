0cafcc130774aac1080d6a9e07c5940a1cb17ac2
ajaypai1971 <ajay.pai1971@yahoo.com>
2018-06-16 16:49:18 -0700

    Create print_datastore.py
    pyvmomi script to print out a datastore name. 
    
    just an example to get a datastore name

---
+++ print_datastore.py
@@ -0,0 +1,25 @@
+# VMware vSphere Python SDK, pyvmomi
+# Simple script to get vCenter server product details
+
+from pyVim.connect import SmartConnect
+import ssl
+
+s = ssl.SSLContext(ssl.PROTOCOL_SSLv23)  # For VC 6.5/6.0 s=ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+s.verify_mode = ssl.CERT_NONE
+
+si = SmartConnect(host="qa-dur-vc-1.datadomain.com", user="corp\paiajay", pwd="DellEmc@1", sslContext=s)
+datacenter = si.content.rootFolder.childEntity[0]
+
+print("Product Name:", datacenter)
+# print("Product Build:", aboutInfo.build)
+# print("Product Unique Id:", aboutInfo.instanceUuid)
+# print("Product Version:", aboutInfo.version)
+# print("Product Base OS:", aboutInfo.osType)
+# print("Product vendor:", aboutInfo.vendor)
+
+
+x = datacenter.vmFolder.childEntity[4]
+
+vm_datastore =x.childEntity[1].datastore[0].name
+
+print("datastore name is:", vm_datastore)
